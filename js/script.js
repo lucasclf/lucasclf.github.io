da4a9453dc1d761b0102bb3646adaba736fa7d32
lucasclf <cqn.lucas@gmail.com>
2021-08-31 15:30:27 -0300

    Inclusão dos demais projetos.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,32 @@ $(document).ready(function() {
 
     $("#btn-coquepet").click(function() {
 
-        $("#textoMonitorInicial").html("<p class='display-4'>CoquePet <span>:</span></p> <p>Página para um serviço de pet shop ficticio.</p> <p>Técnologias utilizadas: HTML, CSS, Bootstrap, JS e jQuery.</p> <p>Visite o site clicando <a href='https://lucasclf.github.io/ProjetoBootStrap-CoquePet/' target='blank'>aqui</a>. :)<span>|</span> </p>")
+        $("#monitor").html("<p class='display-4'>CoquePet <span>:</span></p> <p>Página para um serviço de pet shop ficticio.</p> <p>Técnologias utilizadas: HTML, CSS, Bootstrap, JS e jQuery.</p> <p>Visite o site clicando <a href='https://lucasclf.github.io/ProjetoBootStrap-CoquePet/' target='blank'>aqui</a>. :)<span>|</span> </p>")
 
     }) 
+
+    $("#btn-jayquery").click(function() {
+
+        $("#monitor").html("<p class='display-4'>jQuery <span>:</span></p><p>Página para uma prefeitura ficticia deselvolvida em conjunto de:</p><p><a href='https://github.com/DiNizo' target='blank'>Eduardo</a>, <a href='https://github.com/marianefreitas' target='blank'>Mariane</a>, <a href='https://github.com/wanessalanne' target='blank'> Wanessa</a>, <a href='https://github.com/jhonjonh' target='blank'>John</a>, <a href='https://github.com/Alessa-LO220620' target='blank'>Alessandra</a>.</p> <p>Técnologias utilizadas: HTML, CSS, JS e jQuery.</p> <p>Visite o site clicando <a href='https://lucasclf.github.io/Projeto_Final_JQ-develop/' target='blank'>aqui</a>. :)<span>|</span> </p>")
+
+    })
     
+    $("#btn-convert-code").click(function() {
+
+        $("#monitor").html("<p class='display-4'>ConvertCode <span>:</span></p> <p>Página para uma empresa ficticia deselvolvida em conjunto de:</p><p><a href='https://github.com/mullerdiana' target='blank'>Diana</a>, <a href='https://github.com/Icognita' target='blank'>Mirian</a>, <a href='https://github.com/marciasoalves' target='blank'> Marcia</a>, <a href='https://github.com/yagomathiass' target='blank'>Yago</a>, <a href='https://github.com/ricardonunes28' target='blank'>Ricardo</a>.</p> <p>Técnologias utilizadas: HTML, CSS e JS.</p> <p>Visite o site clicando <a href='https://lucasclf.github.io/Projeto_JS_SoulCodeAcademy/' target='blank'>aqui</a>. :)<span>|</span> </p>")
+
+    }) 
+
+    $("#btn-alimenticio").click(function() {
+
+        $("#monitor").html("<p class='display-4'>CSS <span>:</span></p> <p>Página para uma empresa ficticia deselvolvida em conjunto de:</p><p><a href='https://github.com/mullerdiana' target='blank'>Diana</a>, <a href='https://github.com/mi-cunha' target='blank'>Noemi</a>, <a href='https://github.com/Mateusfmac' target='blank'> Mateus</a>, <a href='https://github.com/Carlojunior800' target='blank'>Carlos</a>. </p> <p>Técnologias utilizadas: HTML e CSS.</p> <p>Visite o site clicando <a href='https://lucasclf.github.io/SoulCodeAcademy/02%20-%20CSS/Aula%2010/Projeto%20alimentos/index.html' target='blank'>aqui</a>. :)<span>|</span> </p>")
+
+    }) 
+
+    $("#btn-soulcine").click(function() {
+
+        $("#monitor").html("<p class='display-4'>SoulCine <span>:</span></p> <p>Página para uma empresa ficticia deselvolvida em conjunto de:</p><p><a href='https://github.com/marinast85' target='blank'>Marina</a>, <a href='https://github.com/jhonjonh' target='blank'>John</a>, <a href='https://github.com/Icognita' target='blank'>Mirian</a>, <a href='https://github.com/pameoliveira' target='blank'> Pamela</a>.</p> <p>Técnologia utilizada: HTML.</p> <p>Visite o site clicando <a href='https://lucasclf.github.io/SoulCodeAcademy/01%20-%20HTML/Aula05/Trabalho%20em%20equipe/' target='blank'>aqui</a>. :)<span>|</span> </p>")
+
+    }) 
+
 })
